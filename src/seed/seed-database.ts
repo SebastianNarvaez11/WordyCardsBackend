1955793467fd16cb89ee4dc1362cd3521dc8957d
Sebastian Narvaez <sebastian.narvaez@aplicando.co>
2024-06-08 10:26:41 -0500

    crud group

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -7,36 +7,38 @@ const main = async () => {
   await prisma.group.deleteMany();
   await prisma.user.deleteMany();
 
-  try {
-    await prisma.$transaction(async (tx) => {
-      const user = await tx.user.create({
-        data: {
-          email: "sebas@gmail.com",
-          name: "Sebastian",
-          password: bcryptjs.hashSync("tatannvrz"),
-        },
-      });
+  // en producción no se pueden ejecutar la transacciones de prims por alguna razón
 
-      await Promise.all(
-        groups.map(async (group) => {
-          await tx.group.create({
-            data: {
-              name: group.name,
-              iconName: group.iconName,
-              userId: user.id,
-              exercises: {
-                createMany: {
-                  data: group.exercises.map((word) => ({
-                    ...word,
-                    userId: user.id,
-                  })),
-                },
+  try {
+    // await prisma.$transaction(async (tx) => {
+    const user = await prisma.user.create({
+      data: {
+        email: "sebas@gmail.com",
+        name: "Sebastian",
+        password: bcryptjs.hashSync("tatannvrz"),
+      },
+    });
+
+    await Promise.all(
+      groups.map(async (group) => {
+        await prisma.group.create({
+          data: {
+            name: group.name,
+            iconName: group.iconName,
+            userId: user.id,
+            exercises: {
+              createMany: {
+                data: group.exercises.map((word) => ({
+                  ...word,
+                  userId: user.id,
+                })),
               },
             },
-          });
-        })
-      );
-    });
+          },
+        });
+      })
+    );
+    // });
   } catch (error) {
     console.log(error);
   }
